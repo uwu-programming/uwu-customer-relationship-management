a6bae9ba7d0dfffd867044a86e354f2379506e0a
Sunshine <yewhang2004@gmail.com>
2025-04-18 21:55:37 +0800

    testing procedure

--- COMP1044_database.sql
+++ COMP1044_database.sql
@@ -1,4 +1,21 @@
-DROP DATABASE IF EXISTS uwuCRM_ABB;
+-- custom delimiter for writing procedure
+DELIMITER $$
 
-CREATE DATABASE uwuCRM_ABB;
-USE uwuCRM_ABB;
+-- check if a procedure with same name exists, if so, drop it
+DROP PROCEDURE IF EXISTS procedure_create_schema_uwuCRM_ABB $$
+-- create a procedure to create the database schema
+CREATE PROCEDURE procedure_create_schema_uwuCRM_ABB()
+BEGIN
+    DECLARE EXIT HANDLER FOR 1007
+        BEGIN
+
+        END;
+    
+    CREATE DATABASE uwuCRM_ABB;
+
+END$$
+
+-- set the delimiter back to default
+DELIMITER ;
+
+CALL procedure_create_schema_uwuCRM_ABB();
