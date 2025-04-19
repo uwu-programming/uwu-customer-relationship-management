73a2bb23f702114f67b34bead9d44cb294ecd7fe
Sunshine <yewhang2004@gmail.com>
2025-04-19 19:32:11 +0800

    add test.sql for insert test data

---
+++ test.sql
@@ -0,0 +1,21 @@
+INSERT INTO user_role
+    VALUES
+        (1, "wdwq");
+
+INSERT INTO user 
+    VALUES
+        (1, "ewqdq", "dd", "dd", 1),
+        (2, "ewqwqdqdwdq", "ddqwd", "dddq", 1);
+
+INSERT INTO individual 
+        (last_name, gender, honorifics)
+    VALUES
+        ("tokoyami", "Female", "Ms."),
+        ("tokino", "Female", "Ms."),
+        ("kiryu", "Female", "Ms.");
+
+INSERT INTO phone
+    VALUES
+        (123414, "Personal", 1),
+        (12344, "Personal", 1),
+        (314144, "Personal", 2);
