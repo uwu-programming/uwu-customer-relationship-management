1f5e0b8669cc62bbdc079b65e55d8a69fadc7124
Sunshine <yewhang2004@gmail.com>
2025-04-23 23:08:09 +0800

    add sort by, only tag, no function yet

--- COMP1044_SRC/src/components/lead_page/lead_display_div.vue
+++ COMP1044_SRC/src/components/lead_page/lead_display_div.vue
@@ -110,6 +110,31 @@
         }
     };
 
+    // check if at least one lead display attribute is selected
+    const need_display = ref(true);
+    const check_need_display = () => {
+        // assume no display
+        need_display.value = false;
+        // loop through the object to see if any display is true
+        for (const key in lead_display_attributes){
+            if (lead_display_attributes[key]['display']['value']){
+                need_display.value = true;
+                break;
+            }
+        }
+    }
+
+    const get_current_display_attribute = () => {
+        const display_attribute = [];
+
+        for (const key in lead_display_attributes){
+            if (lead_display_attributes[key]['display']['value']){
+                display_attribute.push(lead_display_attributes[key]);
+            }
+        }
+        return display_attribute;
+    }
+
     // retrieve all data from TABLE individual JOIN company
     const retrieve_all = async () => {
         try {
@@ -119,10 +144,15 @@
         }
     };
 
+    // record ascending or descending
+    const sort_attribute = ref("default");
+    const sort_order = ref("ASCD");
     // sort the data by order of an attribute
-    const sort_data = async (sort_attribute, sort_by) => {
+    const sort_data = async () => {
         try {
-            response.value = await axios.post("../backend/retrieve_lead_api.php", {sort: true, sort_attribute: sort_attribute, sort_by: sort_by});
+            alert(sort_attribute.value);
+            alert(sort_order.value);
+            //response.value = await axios.post("../backend/retrieve_lead_api.php", {sort: true, sort_attribute: sort_attribute.value, sort_by: sort_order.value});
         } catch (error){
             alert(error);
         }
@@ -133,35 +163,51 @@
 
 <template>
     <div class="flex h-full w-full bg-black overflow-auto">
-        <div class="flex flex-col h-9/10 w-55/100 max-w-max m-2 rounded-2xl border-1 overflow-auto">
-            <div class="flex flex-row w-max bg-fuchsia-500 border-b-2">
-                <div class="flex flex-row w-max" v-for="value in lead_display_attributes" :key="value">
+        <div class="flex flex-col h-9/10 w-55/100 min-w-180 max-w-max m-2 rounded-2xl border-1 overflow-auto">
+            <div class="flex flex-row w-max min-w-180 min-h-8 bg-fuchsia-500 border-b-2">
+                <div class="flex flex-row w-max" v-for="value in lead_display_attributes" :key="value" v-if="need_display">
                     <div v-if="value['display'].value" v-bind:class="(value['class'] + css_class_attributes.title_bar)">{{ value['name'] }}</div>
-                    <span v-else></span>
+                </div>
+                <div class="flex h-full w-full justify-center items-center" v-else>
+                    <span>No attribute selected</span>
                 </div>
             </div>
-            <div class="flex flex-col w-max overflow-y-auto">
-                <button class="w-max bg-sky-500 hover:bg-cyan-300 flex flex-row justify-between border-b-gray-300/80 border-b-1" v-if="response.status==200" v-for="response_value in response.data" :key="response_value" v-bind:id="response_value.individual_id">
+            <div class="flex flex-col w-max overflow-y-auto h-full" v-if="need_display">
+                <button class="w-max min-w-180 min-h-10 bg-sky-500 hover:bg-cyan-300 flex flex-row border-b-gray-300/80 border-b-1" v-if="response.status==200" v-for="response_value in response.data" :key="response_value" v-bind:id="response_value.individual_id">
                     <div class="w-max flex" v-for="value in lead_display_attributes" :key="value">   
                         <span v-if="value['display'].value" v-bind:class="value['class'] + css_class_attributes.display_span">{{ response_value[value['correspond']] }}</span>
                     </div>
                 </button>
             </div>
+            <div class="flex bg-red-400 h-full justify-center items-center" v-else>
+                <span>No content available since no attribute is selected</span>
+            </div>
         </div>
 
-        <div class="flex flex-col h-9/10 w-45/100 bg-violet-500 m-2">
+        <div class="flex flex-col h-9/10 w-45/55 bg-violet-500 m-2">
             <div class="flex flex-col px-2 py-1 bg-rose-700 h-45/100">
                 <div class="">Filter Leads</div>
                 <div><input class="bg-white" type="text"></div>
                 <div class="flex flex-col h-85/100 overflow-auto">
                     <div class="bg-amber-500">Display</div>
                     <div v-for="value in lead_display_attributes" :key="value">
-                        <label :for="value['correspond'] + '_label'">{{ value['name'] }} {{ value['display'].value }}</label>
-                        <input @click="value['display'].value = !value['display'].value" v-model="value['display'].value" :id="value['correspond'] + '_label'" type="checkbox"/>
+                        <label :for="value['correspond'] + '_checkbox'">{{ value['name'] }} {{ value['display'].value }}</label>
+                        <input @click="value['display'].value = !value['display'].value" @input="check_need_display" v-model="value['display'].value" :id="value['correspond'] + '_checkbox'" v-bind:name="value['correspond'] + '_checkbox'" type="checkbox"/>
                     </div>
                 </div>
                 <div class="flex flex-row">
                     <div class="">Sort by</div>
+                    <select @input="sort_data" v-model="sort_attribute" id="select_sort_by">
+                        <option :value="default" v-if="need_display">Default</option>
+                        <option :value="none" v-else>No attribute selected</option>
+                        <option v-for="value in get_current_display_attribute()" :value="value['correspond']">{{ value['name'] }}</option>
+                    </select>
+                    <div class="flex flex-row">
+                        <label for="sort_ascd">Ascending</label>
+                        <input type="radio" v-model="sort_order" :value="ASCD" id="sort_ascd" name="sort_radio" checked/>
+                        <label for="sort_desc">Descending</label>
+                        <input type="radio" v-model="sort_order" :value="DESC" id="sort_desc" name="sort_radio"/>
+                    </div>
                 </div>
             </div>
 
